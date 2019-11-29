4c66ac1fd9c522a4490f635905fc71fe9c724bf8
melaniaromero <57497889+melaniaromero@users.noreply.github.com>
2019-11-28 21:28:05 -0600

    Add files via upload

---
+++ manual.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+void manual ();
+int main (void){
+	manual();
+	return 0;
+
+}
+void manual (){
+	printf("El siguiente programa creado sirve para poder realizar las siguientes funciones, entre ellas se encuentra : 
+		MODIFICAR CONTACTO, BORRAR CONTACTOS, MOSTRAR CONTACTOS, MOSTRAR LOS DATOS DEL CONTACTO, ENLISTAR TODO LOS 
+		CONTACTOS, MOSTRAR LA AYUDA, que en este caso es este mini manual del usuario, Y SALIR. 
+		Simplemente debemos seleccionar una de las siguientes opciones, obviamente escribiendo el número y dándole 
+		enter.
+		El programa automáticamente lo que hará es crear un archivo con los nombres de los
+		contactos que registres,además de los datos personales de estos mismos ya que de no ser, así
+		se perdería todo lo que hayas anotado.
+
+		/");
+		
+
+}
